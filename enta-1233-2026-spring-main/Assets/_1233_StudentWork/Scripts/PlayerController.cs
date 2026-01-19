7ebbda81fd50ee6e81cb93c8275df821fd0ea80f
Arlo <lux.morte@yahoo.com>
2026-01-18 16:45:47 -0800

    Gravity added

--- enta-1233-2026-spring-main/Assets/_1233_StudentWork/Scripts/PlayerController.cs
+++ enta-1233-2026-spring-main/Assets/_1233_StudentWork/Scripts/PlayerController.cs
@@ -12,18 +12,43 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float speed;
 
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityMutiplier = 3.0f;
+    private float _velocity;
+
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
     }
     public void Update()
     {
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+    }
+    private void ApplyGravity()
+    {
+        if (_characterController.isGrounded && _velocity < 0.0f)
+        {
+            _velocity = -1.0f;
+        }
+        else
+        {
+            _velocity += _gravity * gravityMutiplier * Time.deltaTime;
+        }
+
+        _direction.y = _velocity;
+    }
+    private void ApplyRotation()
+    {
         if  (_input.sqrMagnitude == 0) return;
 
         var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
         var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
         transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
-
+    }
+    private void ApplyMovement()
+    {
         _characterController.Move(_direction * speed * Time.deltaTime);
     }
     public void Move(InputAction.CallbackContext context)
